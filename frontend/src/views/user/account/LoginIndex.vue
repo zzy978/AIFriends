240ae14863a7e0c91d17ef9112daee22cdd3c3f2
zzy978 <2727408974@qq.com>
2026-05-10 21:06:51 +0800

    实现注册登录页面

--- frontend/src/views/user/account/LoginIndex.vue
+++ frontend/src/views/user/account/LoginIndex.vue
@@ -1,10 +1,24 @@
-<template>
-登录
-</template>
-
-<script>
+<script setup>
 
 </script>
+
+<template>
+    <div class="flex justify-center mt-30">
+        <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+
+        <label class="label">用户名</label>
+        <input type="email" class="input" placeholder="用户名" />
+
+        <label class="label">密码</label>
+        <input type="password" class="input" placeholder="密码" />
+
+        <button class="btn btn-neutral mt-4">登录</button>
+        <div class="flex justify-end">
+            <RouterLink :to="{name: 'user-account-register-index'}" class="btn btn-sm btn-ghost text-gray-500">注册</RouterLink>
+        </div>
+        </fieldset>
+    </div>
+</template>
 
 <style scoped>
 
--- frontend/src/views/user/account/RegisterIndex.vue
+++ frontend/src/views/user/account/RegisterIndex.vue
@@ -1,10 +1,27 @@
-<template>
-注册
-</template>
-
-<script>
+<script setup>
 
 </script>
+
+<template>
+    <div class="flex justify-center mt-30">
+        <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+
+        <label class="label">用户名</label>
+        <input type="email" class="input" placeholder="用户名" />
+
+        <label class="label">密码</label>
+        <input type="password" class="input" placeholder="密码" />
+
+        <label class="label">确认密码</label>
+        <input type="password" class="input" placeholder="确认密码" />
+
+        <button class="btn btn-neutral mt-4">注册</button>
+        <div class="flex justify-end">
+            <RouterLink :to="{name: 'user-account-login-index'}" class="btn btn-sm btn-ghost text-gray-500">登录</RouterLink>
+        </div>
+        </fieldset>
+    </div>
+</template>
 
 <style scoped>
 
