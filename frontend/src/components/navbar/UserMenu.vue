47c4d9955b1a2e89d8cbb3cea66c4e66e9cbb72e
zzy978 <2727408974@qq.com>
2026-05-13 16:57:58 +0800

    前端对接推出登录api

--- frontend/src/components/navbar/UserMenu.vue
+++ frontend/src/components/navbar/UserMenu.vue
@@ -3,11 +3,29 @@ import { useUserStore } from '@/stores/user.js';
 import UserSpaceIcon from './icons/UserSpaceIcon.vue';
 import UserProfileIcon from './icons/UserProfileIcon.vue';
 import UserLogoutIcon from './icons/UserLogoutIcon.vue';
+import api from '@/js/http/api.js';
+import { useRouter } from 'vue-router';
 
 const user = useUserStore();
+const router = useRouter();
 function closeMenu() {
   const element = document.activeElement
   if (element && element instanceof HTMLElement) element.blur()
+}
+
+async function handleLogout() {
+  try {
+    const res = await api.post('/api/user/account/logout/')
+    if (res.data.result === 'success') {
+      user.Logout()
+      await router.push({
+        name: 'homepage-index'
+      })
+    }
+  }
+  catch (error) {
+    console.log(error)
+  }
 }
 
 </script>
@@ -45,7 +63,7 @@ function closeMenu() {
         </li>
         <li></li>
         <li>
-          <a @click="closeMenu" class="text-sm font-bold py-3">
+          <a @click="handleLogout" class="text-sm font-bold py-3">
             <UserLogoutIcon />
             退出登录
           </a>
