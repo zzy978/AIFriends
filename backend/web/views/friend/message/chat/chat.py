7339b73917ceb44d9adccc5a7e1e1958fb10a5da
zzy978 <2727408974@qq.com>
2026-06-02 14:25:37 +0800

    实现前端播放声音

--- backend/web/views/friend/message/chat/chat.py
+++ backend/web/views/friend/message/chat/chat.py
@@ -73,6 +73,7 @@ class MessageChatView(APIView):
         
         response = StreamingHttpResponse(self.event_stream(app, inputs, friend, message), content_type='text/event-stream')
         response['Cache-Control'] = 'no-cache'
+        response['X-Accel-Buffering'] = 'no'
         return response
     
     async def tts_sender(self, app, inputs, mq, ws, task_id):
@@ -173,7 +174,6 @@ class MessageChatView(APIView):
             msg = mq.get()
             if not msg:
                 break
-            print(msg)
             if msg.get('content', None):
                 full_out += msg['content']
                 yield f"data: {json.dumps({'content': msg['content']}, ensure_ascii=False)}\n\n"
